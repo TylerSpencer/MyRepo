52fa32b6d164403c06fcce9a0d7ca94d95ff500b
TylerSpencer <tyler.spencer@sungard.com>
2015-03-18 12:15:04 -0500

    Added HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int input = 0;
+
+            while (input != 2)
+            {
+                            Console.WriteLine("Enter 1 to display a message or 2 to exit the program: ");
+                input = Convert.ToInt32(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    Console.WriteLine("Please enter a message to display: ");
+                    string message = Console.ReadLine();
+
+                    Console.WriteLine("Your message says '" + message + "'");
+                }
+                else if (input == 2)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+                Console.WriteLine(" ");
+            }
+
+
+
+        }
+    }
+}
